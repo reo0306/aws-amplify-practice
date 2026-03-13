371b5e38ce2b88e150ce80e13bc6e856e1a9c628
reo0306 <reo19840306@gmail.com>
2026-03-14 01:46:02 +0900

    update

--- aws-ai-chat/vite.config.ts
+++ aws-ai-chat/vite.config.ts
@@ -8,4 +8,9 @@ export default defineConfig({
     port: 5173
   },
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    // setupFiles: "./src/setupTests.ts",
+  },
+});
